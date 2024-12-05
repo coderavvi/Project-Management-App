b70f3bf88b5e9183f70c3ca27eebef4cc2c6ea64
coderavvi <iamtabson@gmail.com>
2024-12-05 14:04:38 +0100

    My first commit

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,70 @@
+import { useRef } from "react";
+import Input from "./Input";
+import Modal from "./Modal";
+
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescripttion = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescripttion.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescripttion,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Ooops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className=" w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className=" text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input label="Title" type="text" ref={title} />
+          <Input label="Description" textarea ref={description} />
+          <Input label="Due Date" type="date" ref={dueDate} />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/NewTask.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+export default function NewTask({ onAdd }) {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    function handleChange(event) {
+        setEnteredValue(event.target.value);
+    }
+
+    function handleClick() {
+
+        if(enteredValue.trim() === ''){
+            return;
+        }
+
+        onAdd(enteredValue);
+        setEnteredValue('');
+    }
+
+    return (
+      <div className=" flex items-center gap-4">
+        <input
+          type="text"
+          className=" w-64 px-2 py-1 rounded-sm bg-stone-200"
+          onChange={handleChange}
+          value={enteredValue}
+        />
+        <button
+          className=" text-stone-700 hover:text-stone-950"
+          onClick={handleClick}
+        >
+          Add Task
+        </button>
+      </div>
+    );
+}
